da3cf0712419b71c259034c8770afe5d665d1681
Dani Ijlal Fitriansyah <daniijlalfitriansyah@gmail.com>
2025-06-29 10:53:41 +0700

    Public, Private, Protected

---
+++ Modifier Acces/Public.js
@@ -0,0 +1,11 @@
+class user {
+    constructor(name) {
+        this.name = name; //public
+    }
+
+    sayHello() {
+        console.log('Halo, saya ${this.name}');
+    }
+}
+ const user1 = new user('Dani');
+ console.log(user1.name);
